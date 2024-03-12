72c9bb4c1398143ffd6ee9a19b8d5d91c1aa8641
Rana Salem <ranatasalem@gmail.com>
2024-03-12 11:58:25 +0000

    add changes

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,14 @@
 const { GET } = require('../constants/http-verbs')
+const Wreck = require('@hapi/wreck')
+const { serverConfig } = require('../config')
 
 module.exports = {
   method: GET,
   path: '/',
   options: {
-    handler: (request, h) => {
+    handler: async (request, h) => {
+      const messages = await Wreck.get(`${serverConfig.messagesHost}/messages`)
+      console.log(messages)
       return h.view('home')
     }
   }
