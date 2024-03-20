802af9a8b125154c96db9d132523a9b79f48ced2
Rana Salem <ranatasalem@gmail.com>
2024-03-20 16:37:44 +0000

    update views/routes

--- app/routes/message-view.js
+++ app/routes/message-view.js
@@ -1,9 +1,32 @@
 const { GET } = require('../constants/http-verbs')
+const Wreck = require('@hapi/wreck')
+const { serverConfig } = require('../config')
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  const day = date.getDate()
+  const month = date.toLocaleDateString('default', { month: 'long' })
+  const year = date.getFullYear()
+  return `${day} ${month} ${year}`
+}
 
 module.exports = {
   method: GET,
-  path: '/message-view',
+  path: '/{id}',
   handler: async (request, h) => {
-    return h.view('message-view')
+    try {
+      const messageId = request.params.id
+      const response = await Wreck.get(
+        `${serverConfig.messagesHost}/messages/${messageId}`,
+        { json: true }
+      )
+      const message = {
+        ...response.payload,
+        requestedDate: formatDate(response.payload.requestedDate)
+      }
+      return h.view('message-view', { message })
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
