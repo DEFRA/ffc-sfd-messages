6e106f992c9fca6e44030c2a7cb5b10993dd376e
Rana Salem <ranatasalem@gmail.com>
2024-03-20 15:55:23 +0000

    format requestedDate

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,14 @@ const { GET } = require('../constants/http-verbs')
 const Wreck = require('@hapi/wreck')
 const { serverConfig } = require('../config')
 
+const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  const day = date.getDate()
+  const month = date.toLocaleDateString('default', { month: 'long' })
+  const year = date.getFullYear()
+  return `${day} ${month} ${year}`
+}
+
 module.exports = {
   method: GET,
   path: '/',
@@ -14,7 +22,12 @@ module.exports = {
             json: true
           }
         )
-        return h.view('home', { messages: response.payload })
+        return h.view('home', {
+          messages: response.payload.map((message) => ({
+            ...message,
+            requestedDate: formatDate(message.requestedDate)
+          }))
+        })
       } catch (err) {
         console.error(err)
       }
