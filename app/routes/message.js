f878f59d108f20e34834a97e618dce07792a0fad
Rana Salem <ranatasalem@gmail.com>
2024-04-09 14:57:36 +0100

    update GET messages by id to get by sbi instead (for both message route & view)

--- app/routes/message.js
+++ app/routes/message.js
@@ -6,24 +6,20 @@ const { formatDate } = require('../utils/format-date')
 
 module.exports = {
   method: GET,
-  path: '/message/{id}',
+  path: '/messages/{sbi}',
   options: {
     auth: { strategy: 'jwt', scope: [SFD_VIEW] },
     handler: async (request, h) => {
-      try {
-        const notificationId = request.params.id
-        const response = await Wreck.get(
-          `${serverConfig.messagesHost}/messages/${notificationId}`,
-          { json: true }
-        )
-        const notificationData = {
-          ...response.payload.data,
-          requestedDate: formatDate(response.payload.data.requestedDate)
-        }
-        return h.view('message', { notificationData })
-      } catch (error) {
-        console.error(error)
+      const notificationSbi = request.params.sbi
+      const response = await Wreck.get(
+        `${serverConfig.messagesHost}/messages/${notificationSbi}`,
+        { json: true }
+      )
+      const notificationData = {
+        ...response.payload.data,
+        requestedDate: formatDate(response.payload.data.requestedDate)
       }
+      return h.view('message', { notificationData })
     }
   }
 }
