2c95892bfd7718d4a1f0163745a44037b60f540d
Rana Salem <ranatasalem@gmail.com>
2024-05-09 09:45:01 +0100

    modify view-message page to get data from CosmosDB via GraphQL

--- app/routes/view-message.js
+++ app/routes/view-message.js
@@ -4,29 +4,48 @@ const { serverConfig } = require('../config')
 const { SFD_VIEW } = require('ffc-auth/scopes')
 const { formatDate } = require('../utils/format-date')
 
-module.exports = [
-  {
-    method: GET,
-    path: '/view-message/{sbi}/{id}',
-    options: {
-      auth: { strategy: 'jwt', scope: [SFD_VIEW] },
-      handler: async (request, h) => {
-        try {
-          const notificationSbi = request.params.sbi
-          const notificationId = request.params.id
-          const response = await Wreck.get(
-            `${serverConfig.messagesHost}/messages/${notificationSbi}/${notificationId}`,
-            { json: true }
-          )
-          const notificationData = {
-            ...response.payload.data,
-            requestedDate: formatDate(response.payload.data.requestedDate)
-          }
-          return h.view('view-message', { notificationData })
-        } catch (error) {
-          console.log(error)
-        }
+module.exports = {
+  method: GET,
+  path: '/view-message/{sbi}/{id}',
+  options: {
+    auth: { strategy: 'jwt', scope: [SFD_VIEW] },
+    handler: async (request, h) => {
+      try {
+        const notificationData = await getNotification(request.params.id)
+        // const notificationSbi = request.params.sbi
+        // const notificationId = request.params.id
+        // const response = await Wreck.get(
+        //   `${serverConfig.messageHost}/messages/${notificationSbi}/${notificationId}`,
+        //   { json: true }
+        // )
+        // const notificationData = {
+        //   ...response.payload.data,
+        //   requestedDate: formatDate(response.payload.data.requestedDate)
+        // }
+        return h.view('view-message', { notificationData })
+      } catch (error) {
+        console.log(error)
       }
     }
   }
-]
+}
+
+const getNotification = async (id) => {
+  const query = `query {
+    notification(notificationId: ${id}) {
+      id
+      content
+    }
+  }`
+
+  const { payload } = await Wreck.post(serverConfig.dataHost, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    payload: JSON.stringify({ query }),
+    json: true
+  })
+
+  console.log(payload.data.notification)
+  return payload.data.notification
+}
