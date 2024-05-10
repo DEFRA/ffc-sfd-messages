f3ea43df6c486d11e1cde004f716b9a88dfb58d9
Rana Salem <ranatasalem@gmail.com>
2024-05-10 13:51:53 +0100

    372130 Get messages from data integration service & display on frontend (#7)

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,6 @@
-const Wreck = require('@hapi/wreck')
 const { SFD_VIEW } = require('ffc-auth/scopes')
-const { AUTH_COOKIE_NAME } = require('../constants/cookies')
 const { GET } = require('../constants/http-verbs')
-const { serverConfig } = require('../config')
-const { formatDate } = require('../utils/format-date')
+const { getOrganisation, getNotifications } = require('../data')
 
 module.exports = {
   method: GET,
@@ -13,51 +10,11 @@ module.exports = {
     handler: async (request, h) => {
       try {
         const organisation = await getOrganisation(request)
-        const notifications = await getNotifications(organisation.sbi)
-        return h.view('home', { notifications, organisation })
+        const notificationData = await getNotifications(organisation.sbi)
+        return h.view('home', { notificationData, organisation })
       } catch (error) {
         console.log(error)
       }
     }
   }
-}
-
-const getNotifications = async (sbi) => {
-  const response = await Wreck.get(
-    `${serverConfig.messagesHost}/messages/${sbi}`,
-    {
-      json: true
-    }
-  )
-  return response.payload.data.map((notification) => ({
-    ...notification,
-    requestedDate: formatDate(notification.requestedDate)
-  }))
-}
-
-const getOrganisation = async (request) => {
-  const query = `query {
-          organisation(organisationId: ${request.auth.credentials.organisationId}) {
-            sbi
-            name
-            mobile
-            email
-            address {
-              fullAddress
-            }
-            type
-            legalStatus
-          }
-        }`
-
-  const { payload } = await Wreck.post(serverConfig.dataHost, {
-    headers: {
-      crn: request.auth.credentials.crn,
-      Authorization: request.state[AUTH_COOKIE_NAME],
-      'Content-Type': 'application/json'
-    },
-    payload: JSON.stringify({ query }),
-    json: true
-  })
-  return payload.data.organisation
 }
