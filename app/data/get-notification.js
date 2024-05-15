19527a617dba902866f2cf73d58294de434e0a4e
Rana Salem <ranatasalem@gmail.com>
2024-05-15 23:18:00 +0100

    wrap get-organisation & get-notification in try-catch block

--- app/data/get-notification.js
+++ app/data/get-notification.js
@@ -2,28 +2,32 @@ const Wreck = require('@hapi/wreck')
 const { serverConfig } = require('../config')
 
 const getNotification = async (id) => {
-  const query = `query {
-    notification(id: ${id}) {
-      id
-      scheme
-      tags
-      crn
-      sbi
-      heading
-      body
-      requestedDate
-    }
-  }`
+  try {
+    const query = `query {
+      notification(id: ${id}) {
+        id
+        scheme
+        tags
+        crn
+        sbi
+        heading
+        body
+        requestedDate
+      }
+    }`
 
-  const { payload } = await Wreck.post(serverConfig.dataHost, {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    payload: JSON.stringify({ query }),
-    json: true
-  })
+    const { payload } = await Wreck.post(serverConfig.dataHost, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      payload: JSON.stringify({ query }),
+      json: true
+    })
 
-  return payload.data.notification
+    return payload.data.notification
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = getNotification
--- app/data/get-organisation.js
+++ app/data/get-organisation.js
@@ -3,31 +3,35 @@ const { serverConfig } = require('../config')
 const { AUTH_COOKIE_NAME } = require('../constants/cookies')
 
 const getOrganisation = async (request) => {
-  const query = `query {
-          organisation(organisationId: ${request.auth.credentials.organisationId}) {
-            sbi
-            name
-            mobile
-            email
-            address {
-              fullAddress
+  try {
+    const query = `query {
+            organisation(organisationId: ${request.auth.credentials.organisationId}) {
+              sbi
+              name
+              mobile
+              email
+              address {
+                fullAddress
+              }
+              type
+              legalStatus
             }
-            type
-            legalStatus
-          }
-        }`
+          }`
 
-  const { payload } = await Wreck.post(serverConfig.dataHost, {
-    headers: {
-      crn: request.auth.credentials.crn,
-      Authorization: request.state[AUTH_COOKIE_NAME],
-      'Content-Type': 'application/json'
-    },
-    payload: JSON.stringify({ query }),
-    json: true
-  })
+    const { payload } = await Wreck.post(serverConfig.dataHost, {
+      headers: {
+        crn: request.auth.credentials.crn,
+        Authorization: request.state[AUTH_COOKIE_NAME],
+        'Content-Type': 'application/json'
+      },
+      payload: JSON.stringify({ query }),
+      json: true
+    })
 
-  return payload.data.organisation
+    return payload.data.organisation
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = getOrganisation
